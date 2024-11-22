33aff426d4aaa4b899f5d4c05a27d4672d1c3289
Trilok <tiosuthar@gmail.com>
2024-11-22 10:49:25 +0530

    1st commit

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -51,4 +51,8 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
 
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
+
+backend.add(import('@backstage/plugin-scaffolder-backend-module-cookiecutter'));
+backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 backend.start();
